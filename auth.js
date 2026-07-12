0d572e9a7a8aff4eab7df88dff593a026700d186
traderindian400-arch <traderindian400@gmail.com>
2026-07-12 16:09:40 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,34 @@
 
+// ======================================
+// Smart Picks Daily
+// auth.js - Part 1
+// Firebase Authentication
+// ======================================
+
+import { auth } from "./firebase.js";
+
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+onAuthStateChanged,
+signOut,
+sendPasswordResetEmail,
+sendEmailVerification,
+updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// =========================
+// DOM Elements
+// =========================
+
+const loginForm = document.getElementById("loginForm");
+
+const emailInput = document.getElementById("email");
+
+const passwordInput = document.getElementById("password");
+
+const googleBtn = document.querySelector(".google-btn");
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+console.log("✅ Auth System Loaded");
